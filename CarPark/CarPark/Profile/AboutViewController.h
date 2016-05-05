60a60ccafe1876487ddf20f2118afed0ad925601
nicer <lanou3g@lanou3gdeMac-mini-109.local>
2016-05-05 09:28:17 +0800

    1  修复bug 2  仍然存在不定时崩溃的问题   下拉刷新逻辑没写好   网速慢的时候有时候会崩溃

---
+++ CarPark/CarPark/Profile/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  CarPark
+//
+//  Created by lanou3g on 16/5/3.
+//  Copyright © 2016年 com.lcarpark.zfw. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface AboutViewController : BaseViewController
+
+@end
